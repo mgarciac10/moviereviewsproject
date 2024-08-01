aaf5346615096eaf4ba13b13526ae5aca0bac518
Mateo Garcia Carreno <mgarciac03@outlook.com>
2024-08-01 10:45:36 -0500

    Cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Mateo Garcia'})
+    return render(request, 'home.html', {'name':'Mateo Garcia Carreno'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
