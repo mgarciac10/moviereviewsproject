a87bf89b1f7ec3076c9b21f399fdf7a216650849
Mateo Garcia Carreno <mgarciac03@outlook.com>
2024-08-22 10:22:29 -0500

    Registro del modelo en admin.py

--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .Models import News
 
 # Create your views here.
+def news(request):
+    news = News.objects.all().order_by('-date')
+    return render(request, 'news.html', {'news': news})
