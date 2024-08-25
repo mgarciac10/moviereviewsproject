20401617d1c127f577df64acdf6cdb1b579fc04d
Mateo Garcia Carreno <mgarciac03@outlook.com>
2024-08-25 00:33:08 -0500

    Added the statistics page with a movie per year chart

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 from .models import Movie
 
+import matplotlib.pyplot as plt 
+import matplotlib 
+import io 
+import urllib, base64
+
 # Create your views here.
 
 def home(request):
@@ -19,3 +24,45 @@ def home(request):
 def about(request):
     #return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
+
+def statistics_view_movie_year(request): 
+    matplotlib.use('Agg') 
+    all_movies = Movie.objects.all()
+ 
+    movie_counts_by_year = {} 
+ 
+    for movie in all_movies: 
+        year = movie.year if movie.year else "None" 
+        if year in movie_counts_by_year: 
+            movie_counts_by_year[year] += 1 
+        else: 
+            movie_counts_by_year[year] = 1 
+ 
+    bar_width = 0.5  
+    bar_positions = range(len(movie_counts_by_year)) 
+ 
+    # Crear la gráfica de barras 
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center') 
+ 
+    # Personalizar la gráfica 
+    plt.title('Movies per year') 
+    plt.xlabel('Year') 
+    plt.ylabel('Number of movies') 
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90) 
+ 
+    # Ajustar el espaciado entre las barras 
+    plt.subplots_adjust(bottom=0.3) 
+ 
+    # Guardar la gráfica en un objeto BytesIO 
+    buffer = io.BytesIO() 
+    plt.savefig(buffer, format='png') 
+    buffer.seek(0) 
+    plt.close() 
+    # Convertir la gráfica a base64 
+    image_png = buffer.getvalue() 
+    buffer.close() 
+    graphic = base64.b64encode(image_png) 
+    graphic = graphic.decode('utf-8') 
+    # Renderizar la plantilla statistics.html con la gráfica 
+    return render(request, 'statistics.html', {'movies_per_year': graphic}) 
+
